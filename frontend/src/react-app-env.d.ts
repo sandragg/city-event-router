9241bafc9c237fe826a1edb007d7ad5db697c266
sandragg <sandragg@yandex.ru>
2019-12-28 23:49:44 +0300

    More ymap types

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -2,7 +2,78 @@
 
 import { AnyObject } from "react-yandex-maps";
 
-interface ObjectData extends AnyObject {}
+/* Payload */
+
+interface Range<T> {
+	from: T;
+	to: T;
+}
+
+enum WeekDay {
+	Monday = "Monday",
+	Tuesday = "Tuesday",
+	Thursday = "Thursday",
+	Friday = "Friday",
+	Saturday = "Saturday",
+	Sunday = "Sunday",
+	Everyday = "Everyday"
+}
+
+interface Schedule {
+	Intervals?: Range<string>[];
+	TwentyFourHours?: boolean;
+	[key: WeekDay]: boolean;
+}
+
+interface Category {
+	name: string;
+	class: string;
+}
+
+interface ResponseMetaData {
+	id?: string;
+	name: string;
+	address: string;
+	url?: string;
+	Categories: Category[];
+	Hours: {
+		text: string;
+		Availabilities: Schedule[];
+	};
+}
+
+interface MetroStationData {
+	id: string;
+	type: string;
+	title: string;
+	isUndergroundExit: boolean;
+	undergroundLineMeta: {
+		name: string;
+		color: string;
+	}
+}
+
+interface Rating {
+	ratings: number;
+	reviews: number;
+	score: number;
+}
+
+interface ObjectData extends AnyObject {
+	id?: string;
+	name: string;
+	description: string;
+	type: string;
+	address: string;
+	boundedBy: [number, number][];
+	point: [number, number];
+	url?: string;
+	responseMetaData: ResponseMetaData;
+	rating?: Rating;
+	mtrData?: MetroStationData;
+}
+
+/* Map API */
 
 interface InfoObject {
 	properties: {
@@ -24,6 +95,7 @@ type EventCallback = (e: IEvent) => any;
 
 interface EventManager<T extends string> {
 	add(types: T | T[], callback: EventCallback) : this;
+	remove(types: T | T[], callback: EventCallback) : this;
 	fire(type: T, event?: EntityEvent) : this;
 }
 
