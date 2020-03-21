627441a02db53df943b2ce55ac0a31b174b70c66
sandragg <sandragg@yandex.ru>
2020-03-21 13:41:19 +0300

    Implemented CustomSettings model

---
+++ routing/models/custom-settings.h
@@ -0,0 +1,23 @@
+#ifndef ROUTING_CUSTOM_SETTINGS_H
+#define ROUTING_CUSTOM_SETTINGS_H
+
+#include <iostream>
+#include <vector>
+#include "../timetable/interval.h"
+
+
+struct CustomSettings
+{
+	time_t min_stay_time;
+	time_t late_fee;
+	time_t time_before_open_fee;
+	time_t extra_time_fee;
+
+	/* pair<point id, priority>  */
+	std::vector<std::pair<int, int>> *points_priorities;
+
+	timetable::Interval time_range;
+};
+
+
+#endif //ROUTING_CUSTOM_SETTINGS_H
