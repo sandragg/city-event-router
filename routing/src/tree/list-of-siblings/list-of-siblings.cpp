164270134198b827d3e4a224ac2dfce1aaebcff3
sandragg <sandragg@yandex.ru>
2020-04-11 23:41:41 +0300

    Implemented End and Root methods

--- routing/src/tree/list-of-siblings/list-of-siblings.cpp
+++ routing/src/tree/list-of-siblings/list-of-siblings.cpp
@@ -1,1 +1,27 @@
 #include "list-of-siblings.h"
+using namespace list_of_siblings;
+
+
+template<class _Tp>
+typename Tree<_Tp>::iterator Tree<_Tp>::Root()
+{
+	return iterator(this, root);
+}
+
+template<class _Tp>
+typename Tree<_Tp>::const_iterator Tree<_Tp>::Root() const
+{
+	return const_iterator(this, root);
+}
+
+template<class _Tp>
+typename Tree<_Tp>::iterator Tree<_Tp>::End()
+{
+	return iterator(nullptr);
+}
+
+template<class _Tp>
+typename Tree<_Tp>::const_iterator Tree<_Tp>::End() const
+{
+	return const_iterator(nullptr);
+}
